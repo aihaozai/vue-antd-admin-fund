17842c7ba318333c776a1437f25e68855b9ecba5
aihaozai <1020146023@qq.com>
2021-06-14 19:30:03 +0800

    去掉头部

--- src/components/page/header/PageHeader.vue
+++ src/components/page/header/PageHeader.vue
@@ -8,21 +8,21 @@
           </a-breadcrumb-item>
         </a-breadcrumb>
       </div>
-      <div class="detail">
-        <div class="main">
-          <div class="row">
-            <h1 v-if="showPageTitle && title" class="title">{{title}}</h1>
-            <div class="action"><slot name="action"></slot></div>
-          </div>
-          <div class="row">
-            <div v-if="this.$slots.content" class="content">
-              <div v-if="avatar" class="avatar"><a-avatar :src="avatar" :size="72" /></div>
-              <slot name="content"></slot>
-            </div>
-            <div v-if="this.$slots.extra" class="extra"><slot name="extra"></slot></div>
-          </div>
-        </div>
-      </div>
+<!--      <div class="detail">-->
+<!--        <div class="main">-->
+<!--          <div class="row">-->
+<!--            <h1 v-if="showPageTitle && title" class="title">{{title}}</h1>-->
+<!--            <div class="action"><slot name="action"></slot></div>-->
+<!--          </div>-->
+<!--          <div class="row">-->
+<!--            <div v-if="this.$slots.content" class="content">-->
+<!--              <div v-if="avatar" class="avatar"><a-avatar :src="avatar" :size="72" /></div>-->
+<!--              <slot name="content"></slot>-->
+<!--            </div>-->
+<!--            <div v-if="this.$slots.extra" class="extra"><slot name="extra"></slot></div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
     </div>
   </div>
 </template>
