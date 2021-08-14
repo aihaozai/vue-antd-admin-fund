348872f838b355759c9467565bc1bb9dda943698
aihaozai <44828230+aihaozai@users.noreply.github.com>
2021-08-14 17:01:43 +0800

    Update setting.config.js

--- src/config/default/setting.config.js
+++ src/config/default/setting.config.js
@@ -17,7 +17,7 @@ module.exports = {
   multiPage: false,                     //多页签模式，true:开启，false:不开启
   cachePage: true,                      //是否缓存页面数据，仅多页签模式下生效，true 缓存, false 不缓存
   hideSetting: false,                   //隐藏设置抽屉，true:隐藏，false:不隐藏
-  systemName: '数据分析系统',         //系统名称
+  systemName: '数据分析',         //系统名称
   copyright: '',     //copyright
   asyncRoutes: false,                   //异步加载路由，true:开启，false:不开启
   showPageTitle: true,                  //是否显示页面标题（PageLayout 布局中的页面标题），true:显示，false:不显示
@@ -28,8 +28,8 @@ module.exports = {
     direction: 'left'                   //动画方向，切换页面时动画的方向，参考 ./animate.config.js
   },
   footerLinks: [                        //页面底部链接，{link: '链接地址', name: '名称/显示文字', icon: '图标，支持 ant design vue 图标库'}
-    {link: 'https://pro.ant.design', name: 'Pro首页'},
-    {link: 'https://github.com/iczer/vue-antd-admin', icon: 'github'},
-    {link: 'https://ant.design', name: 'Ant Design'}
+    {link: 'https://beian.miit.gov.cn', name: '粤ICP备2021078843号'},
+    // {link: 'https://github.com/iczer/vue-antd-admin', icon: 'github'},
+    // {link: 'https://ant.design', name: 'Ant Design'}
   ],
 }
