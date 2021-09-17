9660d2b97cc77cdea1ac1c76101f360acfe8f14a
zhenjh <aihaozai@gmail.com>
2021-09-17 18:12:40 +0800

    fix:env

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,11 @@
 //跨域代理前缀
- const API_PROXY_PREFIX='/api'
- const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : API_PROXY_PREFIX
+ const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : process.env.VUE_APP_API_BASE_URL
 // const BASE_URL = process.env.VUE_APP_API_BASE_URL
 module.exports = {
     LOGIN: `${BASE_URL}/login`,
     ROUTES: `${BASE_URL}/routes`,
     GOODS: `${BASE_URL}/goods`,
     GOODS_COLUMNS: `${BASE_URL}/columns`,
-    CURRENT_MENU: `${BASE_URL}/menu/findMenuByCurrentUser'`,
-    CURRENT_ROLE: `${BASE_URL}/role/findRoleByCurrentUser'`,
+    CURRENT_MENU: `${BASE_URL}/auth/menu/findMenuByCurrentUser`,
+    CURRENT_ROLE: `${BASE_URL}/auth/role/findRoleByCurrentUser`,
 }
